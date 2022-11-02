f58a87d913647c14400f16f69346dcf6b3b9608c
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-11-02 22:07:18 +0100

    fork added

--- part1.c
+++ part1.c
@@ -4,46 +4,10 @@
 #include <stdlib.h> // standard library
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
 #define _XOPEN_SOURCE 600
-
-/**
- * @brief 
- * 
- * @param dir 
- */
-void _ls(const char *dir) {
-	
-	struct dirent *d; // dirent object
-	DIR *dh = opendir(dir); //used for navigation
-	int count = 0;
-
-    //if the file in not present or readable
-	if (!dh) {
-		if (errno = ENOENT) {
-			perror("Directory doesn't exist");
-		}
-		else {
-			perror("Unable to read directory");
-		}
-		exit(EXIT_FAILURE);
-	}
-
-	//While the next entry is not readable we will print directory files
-	while ((d = readdir(dh)) != NULL) {
-
-		//If hidden files are found we continue
-		if (d->d_name[0] == '.') {
-			continue;
-        }
-
-		printf("%s\n", d->d_name); //prints the file name or the directory name followed by a \n sign.
-
-		// count++;
-	}
-
-	// printf("%d", count);
-}
 
 /**
  * @brief The function will take arguments and code accordingly to the options provided.
@@ -54,9 +18,49 @@ void _ls(const char *dir) {
  */
 int main(int argc, const char *argv[]) {
 
-	fork();
+	int pipe(int pipefd[2]);
+	int fd1[2]; // Used to send input string from parent
+    int fd2[2]; // Used to send back the number of lines.
+	pid_t p;
+	char input_str[100];
 
-	_ls(".");
+	/*************************THIS PART READS THE FILES(ls)************************/
+	struct dirent *d; // dirent object
+	DIR *dh = opendir("."); //used for navigation
+
+    //if the file in not present or readable
+	if (!dh) {
+		if (errno = ENOENT) { perror("Directory doesn't exist"); }
+		else { perror("Unable to read directory"); }
+		exit(EXIT_FAILURE);
+	}
+
+	//While the next entry is not readable we will print directory files
+	while ((d = readdir(dh)) != NULL) {
+
+		//If hidden files are found we continue
+		if (d->d_name[0] == '.') { continue; }
+
+		printf("%s\n", d->d_name); //prints the file name or the directory name followed by a \n sign.
+	}
+	/*************************THIS PART READS THE FILES(ls)************************/
+
+	
+	if (pipe(fd1) == -1) {
+        fprintf(stderr, "Pipe Failed");
+        return 1;
+    }
+    if (pipe(fd2) == -1) {
+        fprintf(stderr, "Pipe Failed");
+        return 1;
+    }
+
+	p = fork();
+
+	if (p < 0) {
+        fprintf(stderr, "fork Failed");
+        return 1;
+    }
 
 	return 0;
 }
