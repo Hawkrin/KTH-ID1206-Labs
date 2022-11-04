042a56df1be61d0c441bf7eef4b0c8fdaf88b0cd
Maximilian Petersson <maxi.petersson@gmail.com>
2022-11-04 17:53:44 +0100

    parent vs child, vem kommer först?

--- Lab1/pipe.c
+++ Lab1/pipe.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
+#include<sys/wait.h>
 #include <unistd.h>
 
+
+/*
+ *  Parent skapar en child och 
+ */
 void main(void){
     int fd[2];
     char buf;
@@ -20,14 +25,19 @@ void main(void){
         case -1: //Error forking
         break;
         case 0: //Child
+            //for(int i = 0; i < 100000; i++){}
+            printf("child\n");
             char *msg = "Hello World!";
             close(fd[0]); //Stäng läs porten (är det rätt att kalla den för port?) då child ska skriva. 
-            write(fd[1], msg, strlen(msg)); //Argument: peka på den enden av pipen som är skriv, vad som ska skrivas, storleken på meddelandet. 
+            write(fd[1], msg, strlen(msg)); //Argument: peka på den änden av pipen som är skriv, vad som ska skrivas, storleken på meddelandet. 
             close(fd[1]); //Stäng skriv porten. När både läs o skriv är stängda stängs den "virituella filen" (pipen) och child kan terminera. 
         break;
         default: //Parent
+            for(int i = 0; i < 100000; i++){}
+            printf("Parent\n");
             close(fd[1]);
-            while(read(fd[0], &buf, 1) > 0) //Läs 1 byte o spara i buf. read returnerar antal bytes lästa, i vårt fall returnerar den 1. Om den läser, skriv...
+            //wait(NULL); 
+            while(read(fd[0], &buf, 1) > 0) //Läs 1 byte (en char) o spara i buf. read returnerar antal bytes lästa, i vårt fall returnerar den 1. Om den läser, skriv...
                 write(STDOUT_FILENO, &buf, 1); //Skriv till output bufferten (terminalen).
 
             write(STDOUT_FILENO, "\n", 1); //Lägg till en endline.
@@ -36,3 +46,9 @@ void main(void){
     }
 
 }
+/*
+    En fråga?
+Det känns som att child måste ligga före parent i exekveringen då child skriver och parent läser. 
+Så att parent börjar läsa efter att child har skrivit. Mitt test visar dock att det inte spelar någon
+roll vem det är som ligger först, "Hello World!" kommer printas oavsett. 
+*/
