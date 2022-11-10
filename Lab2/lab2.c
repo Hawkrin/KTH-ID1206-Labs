b9322f23657c4ad146a98583ba0d73d45ead7496
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-11-10 18:12:07 +0100

    counter added

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -7,38 +7,52 @@ pthread_mutex_t lock;
 pthread_t thread[3];
 int buffer = 0;
 
-void *anFunction(void* thread) {
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+
+void *anFunction(void *vargp) {
 
     pthread_mutex_lock(&lock);
-
     sleep(1);
-
-    buffer++;
-
     pthread_mutex_unlock(&lock);  
 }
 
 void main() {
     pid_t pid = getpid();
-    int j = 0;
-    int o = 0;
-    int counter = 0;
+    int i, j = 0;
 
-    while(j < 3) {
 
-        pthread_create(&thread[j], NULL, anFunction, NULL);
-        j++;
+    while(i < 3) {
+        pthread_create(&thread[i], NULL, anFunction, (void *)&thread[i]);
+        i++;
     }
+    while(j < 15) {
+        printf("TID: %d, PID: %d, Buffer: %d\n", thread[j % 3] , pid,  buffer);
+        if(thread[j % 3] == thread[0]) {
+            counter1++;
+        }
+        if(thread[j % 3] == thread[1]) {
+            counter2++;
+        }
+        if(thread[j % 3] == thread[2]) {
+            counter3++;
+        }
+        j++;
+        buffer++;
+    }
+
+    printf("TID: %d worked on the buffer %d times\n", thread[0], counter1);
+    printf("TID: %d worked on the buffer %d times\n", thread[1], counter1);
+    printf("TID: %d worked on the buffer %d times", thread[2], counter1);
 
     pthread_join(thread[0], NULL);
     pthread_join(thread[1], NULL);
     pthread_join(thread[2], NULL);
 
     
-    while(o < 15) {
-        printf("TID: %d, PID: %d,  Buffer: %d\n", thread[o % 3] , pid,  buffer);
-        o++;
-    }
+
+    
 
     
 }
