04d0a127f0fc14eabc38b06ffcf7d73c9baacbef
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-11-16 14:19:16 +0100

    lab2 done

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -2,57 +2,67 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdbool.h>
+
+#define NO_OF_THREADS 3
+
+
+/*********************************************************************************
+ * @author
+ * Malcolm Liljedahl, Robin Fransson, Maximilian Petersson
+ * @brief 
+ * In this assignment, you will write a program that creates three threads.
+ * These threads access a shared integer, called buffer, one at a time. The buffer
+ * will initially be set to 0. Each thread should print its thread ID, process ID, and
+ * the buffer’s current value in one statement, then increment the buffer by one.
+ * Use a mutex to ensure this whole process is not interrupted. Have the threads
+ * modify the buffer 15 times. When each thread is done, it should return the
+ * number of times it modified the buffer to the main thread. 
+ ********************************************************************************/
 
 pthread_mutex_t lock;
 pthread_t thread[3];
 int buffer = 0;
 
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
 
 void *anFunction(void *vargp) {
+    
+    pid_t pid = getpid();
+    int instance_counter = 0;
 
-    pthread_mutex_lock(&lock);
-    sleep(1);
-    pthread_mutex_unlock(&lock);  
+    while(true) {
+        pthread_mutex_lock(&lock);
+        if(buffer == 15){
+            pthread_mutex_unlock(&lock);
+            break;
+        }
+        printf("TID: %d, PID: %d Buffer: %d\n", pthread_self() , pid ,  buffer);
+        buffer++;
+        pthread_mutex_unlock(&lock); 
+        
+        instance_counter++;
+    }
+
+    printf("\nTID: %d, worked on buffer, %d times", pthread_self(), instance_counter);
+
+    pthread_exit(0);
 }
 
 void main() {
-    pid_t pid = getpid();
+    
+    pthread_mutex_init(&lock, NULL);
+    
     int i, j = 0;
 
-    while(i < 3) {
-        pthread_create(&thread[i], NULL, anFunction, (void *)&thread[i]);
+    while(i < NO_OF_THREADS) {
+        pthread_create(&thread[i], NULL, anFunction, NULL);
         i++;
     }
-    while(j < 15) {
-        printf("TID: %d, PID: %d, Buffer: %d\n", thread[j % 3] , pid,  buffer);
-        if(thread[j % 3] == thread[0]) {
-            counter1++;
-        }
-        if(thread[j % 3] == thread[1]) {
-            counter2++;
-        }
-        if(thread[j % 3] == thread[2]) {
-            counter3++;
-        }
+
+    while(j < NO_OF_THREADS) {
+        pthread_join(thread[j], NULL);
         j++;
-        buffer++;
     }
 
-    printf("TID: %d worked on the buffer %d times\n", thread[0], counter1);
-    printf("TID: %d worked on the buffer %d times\n", thread[1], counter1);
-    printf("TID: %d worked on the buffer %d times\n", thread[2], counter1);
-    printf("Total buffer accesses: %d", (counter1+counter1+counter3));
-
-    pthread_join(thread[0], NULL);
-    pthread_join(thread[1], NULL);
-    pthread_join(thread[2], NULL);
-
-    
-
-    
-
-    
+    printf("\nTotal buffer accesses: %d", (buffer));
 }
