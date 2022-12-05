7d4f22e598e72159285a4f4993a5008b4ab1e60c
Lillfrallan <76948186+Lillfrallan@users.noreply.github.com>
2022-12-05 14:05:07 +0100

    Update lab3.c

--- Lab3/lab3.c
+++ Lab3/lab3.c
@@ -12,7 +12,7 @@
 #define PAGE_SIZE 256           // Page size in bytes
 #define NO_OF_FRAMES 256        // Number of entries in the backing store?
 #define FRAME_SIZE 256          // Frame size in bytes
-#define NO_OF_TLB_ROW 16        // Number of rows in the TLB
+#define NO_OF_TLB_ROWS 16        // Number of rows in the TLB
 #define NO_OF_TLB_COLUMNS 2     // Number of columns in the TLB
 
 int main() {
@@ -38,14 +38,15 @@ int main() {
         offset,
         page_faults = 0,
         TLB_hits = 0,
+        TLB_counter = 0,
         empty_frame_index = -1,
         no_of_translations = 0, 
         frame_number,
         page_table[NO_OF_PAGES] = {[0 ... (NO_OF_PAGES - 1)] = -1 },  // -1 if free, 0 if allocated
         frame_table[NO_OF_PAGES] = {[0 ... (NO_OF_PAGES - 1)] = -1 }, // -1 if free, 0 if allocated   
-        TLB[NO_OF_TLB_ROW][NO_OF_TLB_COLUMNS];
+        TLB[NO_OF_TLB_ROWS][NO_OF_TLB_COLUMNS];
 
-    memset(TLB, -1, sizeof(TLB[0][0]) * NO_OF_TLB_ROW * NO_OF_TLB_COLUMNS);
+    memset(TLB, -1, sizeof(TLB[0][0]) * NO_OF_TLB_ROWS * NO_OF_TLB_COLUMNS);
 
     address_file = fopen("data/addresses.txt", "r"); 
 	backing_store = fopen("data/BACKING_STORE.bin", "r");
@@ -61,7 +62,7 @@ int main() {
 
          //TLB-hit, obtain frame number
         TLB_hit = false;
-        for (int i = 0; i < NO_OF_TLB_ROW; i++) {
+        for (int i = 0; i < NO_OF_TLB_ROWS; i++) {
             if (TLB[i][0] == page_number) {
                 physical_address = TLB[i][1];
                 TLB_hit = true;
@@ -74,8 +75,9 @@ int main() {
         if(page_table[page_number]!= -1) {
             frame_number = page_table[page_number];
             physical_address = (frame_number * FRAME_SIZE) + offset;
-            TLB[no_of_translations % NO_OF_TLB_ROW][0] = page_number;
-            TLB[no_of_translations % NO_OF_TLB_ROW][1] = physical_address;
+            TLB[TLB_counter % NO_OF_TLB_ROWS][0] = page_number;
+            TLB[TLB_counter % NO_OF_TLB_ROWS][1] = physical_address;
+            TLB_counter++;
         }
         //TLB-miss, pagetable-miss, page-fault	
         else {
@@ -106,12 +108,13 @@ int main() {
             page_table[page_number] = empty_frame_index;	
             physical_address = (empty_frame_index * FRAME_SIZE) + offset;
             frame_table[empty_frame_index] = 0;
-            TLB[no_of_translations % NO_OF_TLB_ROW][0] = page_number;
-            TLB[no_of_translations % NO_OF_TLB_ROW][1] = physical_address;
+            TLB[TLB_counter % NO_OF_TLB_ROWS][0] = page_number;
+            TLB[TLB_counter % NO_OF_TLB_ROWS][1] = physical_address;
+            TLB_counter++;
         }
         byte_value = physical_memory[physical_address];
         no_of_translations++;
-        memset(ch,0,sizeof(ch));
+        memset(ch,0,sizeof(char));
         printf("\nVirtual address: %d,  Physical address: %d, Value: %d", logical_address, physical_address, byte_value);
 
     }
